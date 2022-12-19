d5cf2dece88bd8bc9657b53b1fe5a59b4d9ac853
sreekantha26 <srikanth.nrsa@gmail.com>
2022-12-19 13:48:36 +1100

    Update Test1.js
    fixed the drop down selection

--- cypress/Integration/Examples/Test1.js
+++ cypress/Integration/Examples/Test1.js
@@ -21,12 +21,10 @@ cy.get('#Postcode').type('3163')
 cy.get('#MainPhone').type('03 0431123620')
 cy.get('#WebUrl').type('https://dev-rhipe-signup.azurewebsites.net/')
 cy.get('#Email').type('test@test.com')
-cy.get('#Source')
-cy.get('#IndustryType')
+//dropdown selection
+cy.get('#Source:visible').select('Rhipe Website',{force:true})
+cy.get('#IndustryType').select('IT Consultants',{force:true})
 
-//cy.get('.useanotheraddress').check().should('be.checked')
-//cy.get('.useanotheraddress').uncheck().should('not.be.checked')
-cy.get('[data-layer="Content"]')
 //verify the field
 cy.get('#onboarding-form > :nth-child(7) > h3').should('be.visible')
 //Authorised Signatory Contact(Person who will sign the Terms of Trade and respective Vendor agreements)
@@ -35,8 +33,10 @@ cy.get('#PrimaryContactLastName').type('test')
 cy.get('#PrimaryContactPhone').type('0432311893')
 cy.get('#onboarding-form > :nth-child(8) > tbody > :nth-child(2) > :nth-child(4)')
 cy.get('#PrimaryContactEmail').type('test@test.com')
+//verifying the captcha code element however we are not doing any action here
+cy.get('[style="width: 304px; height: 78px;"] > div > iframe')
 //submit the button here we are not clicking just verifying button as mentioned in Use case
-cy.get('#PrimaryContactEmail')
+cy.get('#submitbutton')
 
 
 

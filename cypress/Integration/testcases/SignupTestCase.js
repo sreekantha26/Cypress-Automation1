5ffe76dd143499bcb8d2d0846d0956df94c1a73f
sreekantha26 <srikanth.nrsa@gmail.com>
2023-01-09 01:56:48 +1100

    test data
    calling test data from fixtures

--- cypress/Integration/testcases/SignupTestCase.js
+++ cypress/Integration/testcases/SignupTestCase.js
@@ -1,11 +1,15 @@
 ///<reference types="cypress" />
 import SignupPageActions from "../../pageobjects/pageactions/SignupPageAction"
+
 describe("Signup Testcase",()=>{
     const singUpPage = new SignupPageActions()
+   
+   
     beforeEach(()=>{
         singUpPage.navigateToURL()
     })
     it("verify submitform  of the SignUpPage",()=>{
+      
         singUpPage.submitSignupPageInfo()
         singUpPage.submitAuthorisedSignatoryContact()
     })
--- cypress/pageobjects/pageactions/SignupPageAction.js
+++ cypress/pageobjects/pageactions/SignupPageAction.js
@@ -1,5 +1,6 @@
 ///<reference types="cypress" />
 import SignupPageElements from "../pageelements/SignupPageElements"
+const data =require("../../fixtures/testdata.json")
 export default class SignupPageActions{
     constructor(){
         globalThis.element = new SignupPageElements()
@@ -9,28 +10,28 @@ export default class SignupPageActions{
         cy.visit('/')
     }
     submitSignupPageInfo(){
-        element.billingEntityNamefield().type("QA")
-        element.legalEntityID().type("QA GROUP")
-        element.addressLine1().type("address1")
-        element.addressLine2().type("address2")
-        element.addressLine3().type("address3")
-        element.city().type("Melbourne")
-        element.state().type("VIC")
-        element.postcode().type("3977")
-        element.mainPhone().type("0432223619")
-        element.webUrl().type("https://dev-rhipe-signup.azurewebsites.net/")
-        element.email().type("test@test.com")
-        element.howDidYouHearAboutRhipe().select('Rhipe Website',{force:true})
-        element.industryType().select('IT Consultants',{force:true})
+       element.billingEntityNamefield().type(data.billingEntityNamefield)
+        element.legalEntityID().type(data.legalEntityID)
+        element.addressLine1().type(data.addressLine1)
+        element.addressLine2().type(data.addressLine2)
+        element.addressLine3().type(data.addressLine3)
+        element.city().type(data.city)
+        element.state().type(data.state)
+        element.postcode().type(data.postcode)
+        element.mainPhone().type(data.phone)
+        element.webUrl().type(data.webURL)
+        element.email().type(data.email)
+        element.howDidYouHearAboutRhipe().select(data.howDidYouHearAboutRhipe,{force:true})
+        element.industryType().select(data.industryType,{force:true})
         //verify the field
         element.authorisedSignatoryText().should('be.visible')
 
     }
     submitAuthorisedSignatoryContact(){
-        element.firstNamefield().type("sample")
-        element.lastNamefield().type("sampledata")
-        element.directPhonefield().type("0432311893")
-        element.PrimaryContactEmail().type("test@test.com")
+        element.firstNamefield().type(data.firstName)
+        element.lastNamefield().type(data.lastName)
+        element.directPhonefield().type(data.directPhone)
+        element.PrimaryContactEmail().type(data.PrimaryContactEmail)
         //verify captcha field 
         element.captcaField().should('be.visible')
         //verify the submit button
